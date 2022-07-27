c7855f82cdc718b592eb615cb12750f4372b63a4
harryguiacorn <fxacorn@gmail.com>
2022-07-27 18:58:15 +0100

    Added Ichimoku plugin for csv

--- runPandas.py
+++ runPandas.py
@@ -2,32 +2,47 @@
 from pandas_datareader import data as pdr
 import matplotlib.pyplot as plt
 from tapy import Indicators
-from TAcharts.indicators.ichimoku import Ichimoku
-from TAcharts.utils.ohlcv import OHLCV
+
+from finta import TA
 
 # initializing Parameters
 start = "2022-01-01"
 end = "today"
-symbols = ["AAPL"]
-  
+symbols = ["AAPL"]  # , "TSLA", "GOOGL"
+list_data = []
+
+f = open('pandas.txt', 'w')
+
 # Getting the data
-data = pdr.get_data_yahoo(symbols, start, end)
-data.to_csv('AAPL.csv')
+for __symbol in symbols:
+    try:
+        data = pdr.get_data_yahoo(__symbol, start, end)
+
+        f.write(__symbol+'\n')
+        f.write(data.corr().to_string())
+        f.write('\n')
+
+        list_data.append(data)
+
+        data.to_csv('data/' + __symbol + '.csv')
+    except Exception as e:
+        print("Error: ")
+        raise Exception("Error getting: ")
+f.close()
 
 # initialising indicators
-""" __i = Indicators(data)
+__i = Indicators(data)
 __i.ichimoku_kinko_hyo(column_name_kijun_sen="K Line")
-data = __i.df
-data.to_csv('AAPL Cloud.csv') """
+__dataCloud = __i.df
+__dataCloud.to_csv('data/AAPL_cloud.csv')
 
 # Display
 """ plt.figure(figsize = (20,10))
-plt.title('Opening Prices from {} to {}'.format(start, end))
+plt.title('Closing Prices from {} to {}'.format(start, end))
 plt.plot(data['Close'])
 plt.show() """
 
-# Plot Cloud Chart
-# __plot = Ichimoku(data)
-# __plot.build(26, 52, 52, 26)
-
-# __plot.plot()
+""" df = pd.read_csv('data/AAPL.csv')
+print(df.head())
+print(TA.ICHIMOKU(df))
+TA.ICHIMOKU(df).to_csv('data/' + __symbol + '-cloud.csv') """
