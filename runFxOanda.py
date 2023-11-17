a5fe700d131946322ebc707fa117379c16d1291b
Harry Gui <fxacorn@gmail.com>
2023-11-17 20:42:41 +0000

    added Oanda Forex API

---
+++ runFxOanda.py
@@ -0,0 +1,104 @@
+from src.mvc import (
+    GetDataForexOanda,
+    # GetIchimokuCloudDataForexOanda,
+    # GetIchimokuCloudDataForexOandaAggregator,
+    # GetIchimokuCloudDataForexOandaMerger,
+    # GetIchimokuKijunDataForexOanda,
+    # GetIchimokuKijunDataForexOandaAggregator,
+    # GetKickerDataForexOanda,
+    # GetKickerDataForexOandaAggregator,
+)
+
+
+fetch_symbols_latest = True
+
+fetch_ForexOanda_1H = True
+
+fetch_ForexOanda_4H = True
+
+fetch_ForexOanda_D = True
+
+fetch_ForexOanda_W = False
+
+fetch_ForexOanda_M = False
+
+fetch_Kicker_intraday = False
+
+
+def main():
+    # Stop script being auto-run by Replit or Gitpod
+    # return
+
+    # ---------------- Forex Oanda ----------------
+
+    _getDataForexOanda = GetDataForexOanda
+    _getDataForexOanda.main(
+        fetch_ForexOanda_1H,
+        fetch_ForexOanda_4H,
+        fetch_ForexOanda_D,
+        fetch_ForexOanda_W,
+        fetch_ForexOanda_M,
+    )
+    return
+    _getIchimokuCloudDataForexOanda = GetIchimokuCloudDataForexOanda
+    _getIchimokuCloudDataForexOanda.main(
+        fetch_ForexOanda_1H,
+        fetch_ForexOanda_D,
+        fetch_ForexOanda_W,
+        fetch_ForexOanda_M,
+    )
+
+    _getIchimokuCloudDataForexOandaAggregator = (
+        GetIchimokuCloudDataForexOandaAggregator
+    )
+    _getIchimokuCloudDataForexOandaAggregator.main(
+        fetch_ForexOanda_1H,
+        fetch_ForexOanda_D,
+        fetch_ForexOanda_W,
+        fetch_ForexOanda_M,
+    )
+
+    _getIchimokuCloudDataForexOandaMerger = (
+        GetIchimokuCloudDataForexOandaMerger
+    )
+    _getIchimokuCloudDataForexOandaMerger.main()
+
+    _getIchimokuKijunDataForexOanda = GetIchimokuKijunDataForexOanda
+    _getIchimokuKijunDataForexOanda.main(
+        fetch_ForexOanda_1H,
+        fetch_ForexOanda_D,
+        fetch_ForexOanda_W,
+        fetch_ForexOanda_M,
+    )
+
+    _getIchimokuKijunDataForexOandaAggregator = (
+        GetIchimokuKijunDataForexOandaAggregator
+    )
+    _getIchimokuKijunDataForexOandaAggregator.main(
+        fetch_ForexOanda_1H,
+        fetch_ForexOanda_D,
+        fetch_ForexOanda_W,
+        fetch_ForexOanda_M,
+    )
+
+    _getKickerDataForexOanda = GetKickerDataForexOanda
+    _getKickerDataForexOanda.main(
+        fetch_Kicker_intraday,
+        fetch_ForexOanda_D,
+        fetch_ForexOanda_W,
+        fetch_ForexOanda_M,
+    )
+
+    _getKickerDataForexOandaAggregator = GetKickerDataForexOandaAggregator
+    _getKickerDataForexOandaAggregator.main(
+        fetch_Kicker_intraday,
+        fetch_ForexOanda_D,
+        fetch_ForexOanda_W,
+        fetch_ForexOanda_M,
+    )
+
+    print(f"Tasks completed.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/mvc/DataOandaAPI.py
@@ -0,0 +1,82 @@
+import requests
+import time
+import json
+import pandas as pd
+
+
+class DataOandaAPI:
+    def __init__(self):
+        self.data = None
+
+    def get_oanda_data(
+        self,
+        instrument="EUR_USD",
+        count=60,
+        granularity="H4",
+        pricingComponent="M",
+    ):
+        url = "https://api-fxpractice.oanda.com/v3/instruments/USD_JPY/candles?count=10&price=A&from=2016-01-01T00%3A00%3A00.000000000Z&granularity=D"
+        url = (
+            "https://api-fxpractice.oanda.com/v3/accounts/101-004-21686905-001"
+        )
+        url = "https://api-fxpractice.oanda.com/v3/accounts/101-004-21686905-001/instruments"
+        url = f"https://api-fxpractice.oanda.com/v3/instruments/{instrument}/candles?{count}&price={pricingComponent}&granularity={granularity}"
+        headers = {
+            "Authorization": "Bearer 3b627083d16f29d1e31994bffbe3f229-5b8b7091a8fe4eeaac8223daebd30a94"
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
+    def save_json(self, filePath="data.json", data=None):
+        # Save data to a JSON file
+        with open(filePath, "w") as f:
+            json.dump(data, f)
+
+    def poll_api(self):
+        while True:
+            self.data = self.get_oanda_data()
+            self.save_json()
+            print(self.data)
+            time.sleep(5)  # Poll every 5 seconds
+
+    def json_to_csv(self, data, filePath="data.csv"):
+        # Assuming json_string is your JSON data
+        # print(data)
+        json_data = data.copy()
+
+        # Extracting the 'candles' data
+        candles = json_data["candles"]
+
+        # Convert the candle data to a DataFrame
+        df = pd.DataFrame(
+            [candle["mid"] for candle in candles],
+            index=pd.to_datetime(
+                [candle["time"] for candle in candles], utc=True
+            ),
+        )
+        # print(df)
+
+        # Rename the columns
+        df.columns = ["Open", "High", "Low", "Close"]
+
+        # Add a column for 'Datetime' and set the index values
+        df.insert(0, "Datetime", df.index)
+
+        # Export the DataFrame to a CSV file
+        df.to_csv(filePath, index=False)
+
+        # print(df)
+
+
+if __name__ == "__main__":
+    api = DataOandaAPI()
+    # poll_api()
+    data = api.get_oanda_data()
+    # data = get_oanda_data()
+    api.save_json(data=data)
+    print(data)
+
+    api.json_to_csv(data)
