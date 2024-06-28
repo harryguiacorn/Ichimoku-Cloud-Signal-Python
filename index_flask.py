905481cf00ff2dd7391b22e9af62a6d0c43a9f72
Harry Gui <fxacorn@gmail.com>
2024-06-28 11:01:42 +0100

    updates titles

---
+++ index_flask.py
@@ -0,0 +1,24 @@
+from flask import Flask, send_file
+import subprocess
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return send_file("index.html")
+
+
+@app.route("/bitfinex")
+def bitfinex():
+    return send_file("output\sum\Bitfinex-sum-cloud-tkx-merged.csv.html")
+
+
+@app.route("/update")
+def update():
+    subprocess.call([".\env\Scripts\python", "runBitfinex.py"])
+    return "Updated"
+
+
+if __name__ == "__main__":
+    app.run()
