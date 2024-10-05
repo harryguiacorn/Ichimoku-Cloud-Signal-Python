f15407d573325d24a2ae9ef1b7d11628ceacdaa5
Harry Gui <fxacorn@gmail.com>
2024-10-05 22:03:50 +0100

    Update html_creator.py

--- src/mvc/html_creator.py
+++ src/mvc/html_creator.py
@@ -29,11 +29,12 @@ class TableGenerator:
           <title>{str_title}</title>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
           <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
-
+          <link rel="icon" type="image/x-icon" href="favicon/favicon.ico">
+          
           <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
           <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
           <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
-
+          
           <style>
               .highlight-positive {{background-color: #c6efce !important;}}
               .highlight-negative {{background-color: #f8d7da !important;}}
