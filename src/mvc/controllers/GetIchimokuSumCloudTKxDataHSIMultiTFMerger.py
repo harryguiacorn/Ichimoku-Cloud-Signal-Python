876f2cf6003e0e3f0108a3b55f7a458c4d66a3c6
Harry Gui <fxacorn@gmail.com>
2024-10-19 13:30:23 +0100

    fixed favicon in sub scan pages

--- src/mvc/controllers/GetIchimokuSumCloudTKxDataHSIMultiTFMerger.py
+++ src/mvc/controllers/GetIchimokuSumCloudTKxDataHSIMultiTFMerger.py
@@ -20,7 +20,7 @@ def main(run_merger=True):
             [
                 "Total Score Sum",
             ],
-            "HSI Cloud Scan",
+            "Hang Seng Cloud Scan",
         )
         _control = Control(_model, View())
         _control.main()
--- src/mvc/html_creator.py
+++ src/mvc/html_creator.py
@@ -29,7 +29,7 @@ class TableGenerator:
           <title>{str_title}</title>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
           <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
-          <link rel="icon" type="image/x-icon" href="favicon/favicon.ico">
+          <link rel="icon" type="image/x-icon" href="../../favicon/favicon.ico">
           
           <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
           <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
