4d3b8db5a15eafb3270658ddd8131e4ab7eb915e
Harry Gui <fxacorn@gmail.com>
2025-12-02 02:25:48 +0000

    Add main orchestration script and new log files
    Introduced a comprehensive main.py script to orchestrate data fetching and processing for multiple financial instruments and sources. Added new log files for various data runs, and removed the unused runBitfinex import. This update centralizes execution and improves maintainability.

--- scripts/main.py
+++ scripts/main.py
@@ -16,7 +16,6 @@ from scripts import (
     runCurrencyFutures,
     runSPX500,
     runOanda,
-    runBitfinex,
     runSPDR_ETFs,
     runKraken,
     runHSI,
@@ -99,3 +98,218 @@ run_Multi_TimeFrame_Merger_CurrencyFutures = True
 run_Multi_TimeFrame_Merger_Oanda = True
 run_Multi_TimeFrame_Merger_Bitfinex = True
 run_Multi_TimeFrame_Merger_Kraken = True
+run_Multi_TimeFrame_Merger_SPDR_ETFs = True
+run_Multi_TimeFrame_Merger_HSI = True
+
+fetch_kijun_analysis = False
+
+fetch_kicker = False
+
+# Use "Datetime" for Yahoo intraday data,
+# "Date" for D, W, M data.
+# Use "Datetime" for all Oanda data.
+fetch_Kicker_use_datetime_format = False
+
+
+def main():
+    # Run the main class and save output
+    # python main.py > output_main.txt
+
+    # Stop script being auto-run by Replit or Gitpod
+    # return
+
+    # Time counter
+    london_tz_start = timezone("Europe/London")
+    time_start = datetime.now(london_tz_start)
+    time_start_formatted = time_start.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Main Task begins at: {time_start_formatted} [UK]")
+
+    # ---------------- Oanda ----------------
+
+    _runOanda = runOanda
+    _runOanda.main(
+        fetch_Oanda_1H,
+        fetch_Oanda_4H,
+        fetch_Oanda_D,
+        fetch_Oanda_W,
+        fetch_Oanda_M,
+        fetch_kijun_analysis,
+        fetch_Kicker_use_datetime_format,
+        run_Multi_TimeFrame_Merger_Oanda,
+        fetch_kicker,
+    )
+    print("\nTasks completed.")
+
+    # ---------------- Dow Jones 30 ----------------
+
+    _runDJ30 = runDJ30
+    _runDJ30.main(
+        fetch_symbols_latest_DJ30,
+        fetch_DJ30_1H,
+        fetch_DJ30_D,
+        fetch_DJ30_W,
+        fetch_DJ30_M,
+        fetch_kijun_analysis,
+        fetch_Kicker_use_datetime_format,
+        run_Multi_TimeFrame_Merger_DJ30,
+        fetch_kicker,
+    )
+
+    # ---------------- Nasdaq 100 ----------------
+
+    _runNas100 = runNas100
+    _runNas100.main(
+        fetch_symbols_latest_Nas100,
+        fetch_Nas100_1H,
+        fetch_Nas100_D,
+        fetch_Nas100_W,
+        fetch_Nas100_M,
+        fetch_kijun_analysis,
+        fetch_Kicker_use_datetime_format,
+        run_Multi_TimeFrame_Merger_Nas100,
+        fetch_kicker,
+    )
+
+    # ---------------- S&P 500 ----------------
+
+    _runSPX500 = runSPX500
+    _runSPX500.main(
+        fetch_SPX500_1H,
+        fetch_SPX500_D,
+        fetch_SPX500_W,
+        fetch_SPX500_M,
+        fetch_kijun_analysis,
+        fetch_Kicker_use_datetime_format,
+        run_Multi_TimeFrame_Merger_SPX500,
+        fetch_kicker,
+    )
+
+    # ---------------- SPDR ETFs ----------------
+
+    _runSPDR_ETFs = runSPDR_ETFs
+    _runSPDR_ETFs.main(
+        False,
+        fetch_SPDR_ETFs_1H,
+        fetch_SPDR_ETFs_D,
+        fetch_SPDR_ETFs_W,
+        fetch_SPDR_ETFs_M,
+        fetch_kijun_analysis,
+        fetch_Kicker_use_datetime_format,
+        run_Multi_TimeFrame_Merger_SPDR_ETFs,
+        fetch_kicker,
+    )
+
+    # ---------------- Kraken ----------------
+
+    _runKraken = runKraken
+    _runKraken.main(
+        fetch_Kraken_1H,
+        fetch_Kraken_4H,
+        fetch_Kraken_D,
+        fetch_Kraken_W,
+        fetch_Kraken_M,
+        fetch_Kicker_use_datetime_format,
+        run_Multi_TimeFrame_Merger_Kraken,
+        fetch_kicker,
+    )
+
+    # ---------------- FTSE 100 ----------------
+
+    _runFTSE100 = runFTSE100
+    _runFTSE100.main(
+        fetch_symbols_latest_FTSE100,
+        fetch_FTSE100_1H,
+        fetch_FTSE100_D,
+        fetch_FTSE100_W,
+        fetch_FTSE100_M,
+        fetch_kijun_analysis,
+        fetch_Kicker_use_datetime_format,
+        run_Multi_TimeFrame_Merger_FTSE100,
+        fetch_kicker,
+    )
+
+    # ---------------- FTSE 250 ----------------
+
+    _runFTSE250 = runFTSE250
+    _runFTSE250.main(
+        fetch_symbols_latest_FTSE250,
+        fetch_FTSE250_1H,
+        fetch_FTSE250_D,
+        fetch_FTSE250_W,
+        fetch_FTSE250_M,
+        fetch_kijun_analysis,
+        fetch_Kicker_use_datetime_format,
+        run_Multi_TimeFrame_Merger_FTSE250,
+        fetch_kicker,
+    )
+
+    # ---------------- Futures ----------------
+
+    _runFutures = runFutures
+    _runFutures.main(
+        fetch_symbols_latest_Futures,
+        fetch_Futures_1H,
+        fetch_Futures_D,
+        fetch_Futures_W,
+        fetch_Futures_M,
+        fetch_kijun_analysis,
+        fetch_Kicker_use_datetime_format,
+        run_Multi_TimeFrame_Merger_Futures,
+        fetch_kicker,
+    )
+
+    # ---------------- Hang Seng Index ----------------
+    _runHSI = runHSI
+    _runHSI.main(
+        fetch_symbols_latest_HSI,
+        fetch_HSI_1H,
+        fetch_HSI_D,
+        fetch_HSI_W,
+        fetch_HSI_M,
+        fetch_kijun_analysis,
+        fetch_Kicker_use_datetime_format,
+        run_Multi_TimeFrame_Merger_HSI,
+        fetch_kicker,
+    )
+
+    # ---------------- Futures Currency ----------------
+
+    _runCurrencyFutures = runCurrencyFutures
+    _runCurrencyFutures.main(
+        fetch_symbols_latest_CurrencyFutures,
+        fetch_CurrencyFutures_1H,
+        fetch_CurrencyFutures_D,
+        fetch_CurrencyFutures_W,
+        fetch_CurrencyFutures_M,
+        fetch_kijun_analysis,
+        fetch_Kicker_use_datetime_format,
+        run_Multi_TimeFrame_Merger_CurrencyFutures,
+        fetch_kicker,
+    )
+
+    # ---------------- Bitfinex ----------------
+
+    """ _runBitfinex = runBitfinex
+    _runBitfinex.main(
+        fetch_Bitfinex_1H,
+        fetch_Bitfinex_4H,
+        fetch_Bitfinex_D,
+        fetch_Bitfinex_W,
+        fetch_Bitfinex_M,
+        fetch_Kicker_use_datetime_format,
+        run_Multi_TimeFrame_Merger_Bitfinex,
+        fetch_kicker,
+    ) """
+
+    # calculate time elapsed
+    london_tz_finish = timezone("Europe/London")
+    time_finish = datetime.now(london_tz_finish)
+    time_elapsed = time_finish - time_start
+    time_finish_formatted = time_finish.strftime("%Y-%m-%d %H:%M:%S")
+    print(
+        f"\nMain Tasks completed at {time_finish_formatted} [UK] (Time elapsed: {time_elapsed})",
+    )
+
+
+if __name__ == "__main__":
+    main()
